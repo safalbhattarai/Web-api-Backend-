a220d83debbfed8cea010e5db9543df703f8d1f7
safalbhattarai <126142996+safalbhattarai@users.noreply.github.com>
2025-03-07 00:18:36 +0545

    Adding food model.js

--- models/foodModel.js
+++ models/foodModel.js
@@ -1,3 +1,23 @@
+// import mongoose from "mongoose";
+
+// const foodSchema = new mongoose.Schema({
+//     name: { type: String, required: true },
+//     description: { type: String, required: true },
+//     price: { type: Number, required: true},
+//     image: { type: String, required: true },
+//     category:{ type:String, required:true}
+// })
+
+// const foodModel = mongoose.models.food || mongoose.model("food", foodSchema);
+// export default foodModel;
+
+
+
+
+
+
+
+
 import mongoose from "mongoose";
 
 const foodSchema = new mongoose.Schema({
@@ -10,3 +30,13 @@ const foodSchema = new mongoose.Schema({
 
 const foodModel = mongoose.models.food || mongoose.model("food", foodSchema);
 export default foodModel;
+
+
+
+
+
+
+
+
+
+
