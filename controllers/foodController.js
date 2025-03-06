356765903645597b98a33169b58ce3abd7a50d61
safalbhattarai <126142996+safalbhattarai@users.noreply.github.com>
2025-03-07 00:15:18 +0545

    adding order controller .js

--- controllers/foodController.js
+++ controllers/foodController.js
@@ -1,124 +1,3 @@
-// import foodModel from "../models/foodModel.js";
-// import fs from 'fs'
-
-// // all food list
-// const listFood = async (req, res) => {
-//     try {
-//         const foods = await foodModel.find({})
-//         res.json({ success: true, data: foods })
-//     } catch (error) {
-//         console.log(error);
-//         res.json({ success: false, message: "Error" })
-//     }
-
-// }
-
-// // add food
-// const addFood = async (req, res) => {
-
-//     let image_filename = `${req.file.filename}`
-
-//     const food = new foodModel({
-//         name: req.body.name,
-//         description: req.body.description,
-//         price: req.body.price,
-//         category:req.body.category,
-//         image: image_filename,
-//     })
-//     try {
-//         await food.save();
-//         res.json({ success: true, message: "Food Added" })
-//     } catch (error) {
-//         console.log(error);
-//         res.json({ success: false, message: "Error" })
-//     }
-// }
-
-// // delete food
-// const removeFood = async (req, res) => {
-//     try {
-
-//         const food = await foodModel.findById(req.body.id);
-//         fs.unlink(`uploads/${food.image}`, () => { })
-
-//         await foodModel.findByIdAndDelete(req.body.id)
-//         res.json({ success: true, message: "Food Removed" })
-
-//     } catch (error) {
-//         console.log(error);
-//         res.json({ success: false, message: "Error" })
-//     }
-
-// }
-
-// export { listFood, addFood, removeFood }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 import foodModel from "../models/foodModel.js";
 import fs from 'fs'
 
@@ -133,7 +12,8 @@ const listFood = async (req, res) => {
     }
 
 }
-/ add food
+
+// add food
 const addFood = async (req, res) => {
 
     let image_filename = `${req.file.filename}`
@@ -172,6 +52,68 @@ const removeFood = async (req, res) => {
 }
 
 export { listFood, addFood, removeFood }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- controllers/orderController.js
+++ controllers/orderController.js
@@ -32,6 +32,12 @@ const placeOrder = async (req, res) => {
             quantity: item.quantity
           }))
 
+
+
+
+
+
+
         line_items.push({
             price_data:{
                 currency:"inr",
@@ -109,3 +115,205 @@ const verifyOrder = async (req, res) => {
 }
 
 export { placeOrder, listOrders, userOrders, updateStatus ,verifyOrder }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import orderModel from "../models/orderModel.js";
+import userModel from "../models/userModel.js"
+import Stripe from "stripe";
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+
+// config variables
+const currency = "inr";
+const deliveryCharge = 50;
+const frontend_URL = 'https://tomato-frontend-ds0g.onrender.com';
+
+// Placing User Order for Frontend
+const placeOrder = async (req, res) => {
+
+    try {
+        const newOrder = new orderModel({
+            userId: req.body.userId,
+            items: req.body.items,
+            amount: req.body.amount,
+            address: req.body.address,
+        })
+        await newOrder.save();
+        await userModel.findByIdAndUpdate(req.body.userId, { cartData: {} });
+
+        const line_items = req.body.items.map((item) => ({
+            price_data: {
+              currency: "inr",
+              product_data: {
+                name: item.name
+              },
+              unit_amount: item.price*100*80
+            },
+            quantity: item.quantity
+          }))
