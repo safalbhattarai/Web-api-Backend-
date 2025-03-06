dbb91ecbab23b97a0873655b60045aac2fc233eb
safalbhattarai <126142996+safalbhattarai@users.noreply.github.com>
2025-03-07 00:19:18 +0545

    Adding ordermodelcode

--- models/userModel.js
+++ models/userModel.js
@@ -1,3 +1,50 @@
+// import mongoose from "mongoose";
+
+// const userSchema = new mongoose.Schema({
+//     name: { type: String, required: true },
+//     email: { type: String, required: true, unique: true },
+//     password: { type: String, required: true },
+//     cartData:{type:Object,default:{}}
+// }, { minimize: false })
+
+// const userModel = mongoose.models.user || mongoose.model("user", userSchema);
+// export default userModel;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
